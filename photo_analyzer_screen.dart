d1f110ba759945eda7683be2b4ed1d845ba3e224
Aradhya Mishra <odoisveryverygood@gmail.com>
2025-12-01 18:36:56 -0800

    Create PhotoAnalyzerScreen widget

---
+++ photo_analyzer_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PhotoAnalyzerScreen extends StatelessWidget {
+  const PhotoAnalyzerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Photo Analyzer Screen")),
+    );
+  }
+}
