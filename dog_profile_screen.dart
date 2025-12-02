9b0b9649f6829d66140ab247e4715c64fbc52b57
Aradhya Mishra <odoisveryverygood@gmail.com>
2025-12-01 18:33:28 -0800

    Create DogProfileScreen widget

---
+++ dog_profile_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DogProfileScreen extends StatelessWidget {
+  const DogProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Dog Profile Screen")),
+    );
+  }
+}
